083a8c785593ea75db415c969f1c2b06a7c7850f
Joe <joe.rechenmacher@gmail.com>
2022-06-03 22:03:26 -0500

    Initial Commit

---
+++ Transit/TransitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitApp.swift
+//  Transit
+//
+//  Created by Joseph Rechenmacher on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TransitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
